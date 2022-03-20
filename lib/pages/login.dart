cc2211eb9b343ab78c676fe25d3981e9058672a3
Buluthan İnan <buluthaninan.dev@gmail.com>
2022-03-20 16:07:25 +0300

    chekckbox and simple login by id

---
+++ lib/pages/login.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'adminPage.dart';
+
+class Login extends StatefulWidget{
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login>{
+  TextEditingController _id = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: TextFormField(
+                controller: _id,
+                decoration: InputDecoration(
+                  hintText: "Enter your ID",
+                  labelText: "ID",
+                ),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+            MaterialButton(
+              color: Colors.blue,
+              child: Text("Login In"),
+              onPressed: () {
+                if( _id.text.toString() == "1"  ){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> AdminPage(),
+                  ),
+                  );
+                }
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/userPage.dart
@@ -0,0 +1,76 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class UserPage extends StatefulWidget{
+  @override
+  _UserPageState createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage>{
+  bool _isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("User"),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            LabeledCheckbox(
+              label: 'This is the label text',
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              value: _isSelected,
+              onChanged: (bool newValue) {
+                setState(() {
+                  _isSelected = newValue;
+                });
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LabeledCheckbox extends StatelessWidget {
+  const LabeledCheckbox({
+    Key? key,
+    required this.label,
+    required this.padding,
+    required this.value,
+    required this.onChanged,
+  }) : super(key: key);
+
+  final String label;
+  final EdgeInsets padding;
+  final bool value;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        onChanged(!value);
+      },
+      child: Padding(
+        padding: padding,
+        child: Row(
+          children: <Widget>[
+            Expanded(child: Text(label)),
+            Checkbox(
+              value: value,
+              onChanged: (bool? newValue) {
+                onChanged(newValue!);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
